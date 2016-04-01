648e54d1f131b987b997abd87c322bdc3d979dcc
jiyuan he <hebaihe55@qq.com>
2016-04-01 12:00:32 +0800

    demo1

--- demo1/Controllers/WechatController.cs
+++ demo1/Controllers/WechatController.cs
@@ -14,7 +14,7 @@ namespace demo1.Controllers
         // GET: Wechat
         public ActionResult Index()
         {
-            return View();
+            return View();   
         }
 
         public string  Api(string echostr)
